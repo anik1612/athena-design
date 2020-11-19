b68dfd42d4f5609c6860d844941d3e1cd0e4f0bb
basir5101 <basir.bsmrstu@gmail.com>
2020-11-19 19:13:46 +0600

    footer section done

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Achievement from '../../components/Achievement/Achievement';
 import Contact from '../../components/Contact/Contact';
+import Footer from '../../components/Footer/Footer';
 import Navbar from '../../components/Navbar/Navbar';
 import RunningProject from '../../components/RunningProject/RunningProject';
 import Team from '../../components/Teams/Team';
@@ -13,6 +14,7 @@ const Home = () => {
             <Achievement />
             <Team/>
             <Contact/>
+            <Footer/>
         </>
     );
 };
