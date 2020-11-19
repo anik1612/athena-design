b230b23200ca2dac128fd9317dd21fd2bd0176d2
Anik <aniksarker1612@gmail.com>
2020-11-19 21:23:20 +0800

    error fixed

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import Achievement from '../../components/Achievement/Achievement';
 import Contact from '../../components/Contact/Contact';
-<<<<<<< HEAD
 import Footer from '../../components/Footer/Footer';
-=======
 import HomeBanner from '../../components/HomeBanner/HomeBanner';
->>>>>>> master
 import Navbar from '../../components/Navbar/Navbar';
 import RunningProject from '../../components/RunningProject/RunningProject';
 import Team from '../../components/Teams/Team';
@@ -21,7 +18,7 @@ const Home = () => {
             <Achievement />
             <Team/>
             <Contact/>
-            <Footer/>
+            <Footer />
         </>
     );
 };
