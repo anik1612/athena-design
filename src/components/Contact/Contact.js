476ed029993abf353aba32e2cb1a460a5700a769
Anik <aniksarker1612@gmail.com>
2020-11-20 16:11:46 +0800

    mobile responsive view look better than before

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,12 +5,12 @@ const Contact = () => {
     return (
         <section className = 'container contact mt-5 mb-5'>
             <div className="text-center">
-                <h2 className='mb-2'>Get Your design right now</h2>
-                <p>Be the first know our latest offers and updates!</p>
+                <h2 className='mb-4'>Get Your design right now</h2>
+                <p style={{color: '#8D8D8D'}}>Be the first know our latest offers and updates!</p>
             </div>
             <div className = 'row justify-content-center'>
                 <div className="input-group mb-3 col-md-6 mt-4">
-                    <input type="text" className="form-control" placeholder="Enter your email address" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                    <input type="text" className="form-control" style={{height:'50px'}} placeholder="Enter your email address" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
                     <div className="input-group-append">
                         <button className="btn" type="button">Get Started</button>
                     </div>
--- src/components/RunningProject/RunningProject.js
+++ src/components/RunningProject/RunningProject.js
@@ -4,15 +4,18 @@ import manWithPc from '../../images/Illustration/20 [Converted]@2x.png'
 
 const RunningProject = () => {
     return (
-        <div className='container mb-5 pb-5 mt-5'>
+        <div className='container mb-5 pb-5 mt-5 pt-5'>
             <div className='row d-flex justify-content-between align-items-center'>
-                <div className='col-md-6'>
-                    <img src={manWithPc} height='400px' alt="illustration-img" />
+                <div className='col-md-5 mb-3'>
+                    <img src={manWithPc} className='img-fluid' alt="illustration-img" />
                 </div>
-                <div className='col-md-6'>
+                <div className='col-md-6 mb-3'>
                     <h1>Stay Running {'&'} Project</h1>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letter.</p>
-                    <button className="btn-custom text-white btn px-4 py-3">
+                    <p
+                        style={{ color: '#8D8D8D' }}
+                    >
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letter.</p>
+                    <button className="btn-custom text-white btn px-4 py-3 mt-3">
                         Contact Us
                     </button>
                 </div>
